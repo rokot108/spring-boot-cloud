8dac6b075ae1a5e60b5d2f40a199c00adc58ac5a
rokot108 <38143263+rokot108@users.noreply.github.com>
2019-01-28 15:04:55 +0300

    initial-commit

---
+++ src/main/java/ru/volkov/spring_cloud_config_server/SpringCloudConfigServerApplication.java
@@ -0,0 +1,14 @@
+package ru.volkov.spring_cloud_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+    }
+
+}
+
